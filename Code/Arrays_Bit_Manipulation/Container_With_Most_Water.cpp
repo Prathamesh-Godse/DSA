346236d06e4317d720182c29eced37de38be5342
Prathamesh-Godse <121411511+Prathamesh-Godse@users.noreply.github.com>
2025-07-30 14:31:47 +0530

    Update Container_With_Most_Water.cpp

--- Code/Arrays_Bit_Manipulation/Container_With_Most_Water.cpp
+++ Code/Arrays_Bit_Manipulation/Container_With_Most_Water.cpp
@@ -1,1 +1,23 @@
+class Solution {
+public:
+    int maxArea(vector<int>& height) {
+        int left = 0;
+        int right = height.size() - 1;
+        int maxWater = 0;
 
+        while (left < right) {
+            int heightMin = min(height[left], height[right]);
+            int width = right - left;
+            int currentArea = heightMin * width;
+            maxWater = max(maxWater, currentArea);
+
+            if (height[left] < height[right]) {
+                ++left;
+            } else {
+                --right;
+            }
+        }
+
+        return maxWater;
+    }
+};
