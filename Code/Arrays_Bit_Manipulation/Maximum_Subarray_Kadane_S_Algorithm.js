4f3a5d2ec71af91ac12855ef92a5e06799a7ca60
Prathamesh Godse <121411511+Prathamesh-Godse@users.noreply.github.com>
2025-08-02 12:59:12 +0530

    Create Maximum_Subarray_Kadane_S_Algorithm.js

---
+++ Code/Arrays_Bit_Manipulation/Maximum_Subarray_Kadane_S_Algorithm.js
@@ -0,0 +1,16 @@
+function maxSubArray(nums) {
+    let currentSum = nums[0];
+    let maxSum = nums[0];
+
+    for (let i = 1; i < nums.length; i++) {
+        currentSum = Math.max(nums[i], currentSum + nums[i]);
+        maxSum = Math.max(maxSum, currentSum);
+    }
+
+    return maxSum;
+}
+
+// Test input
+const nums = [-2, 1, -3, 4, -1, 2, 1, -5, 4];
+const result = maxSubArray(nums);
+console.log("Maximum Subarray Sum:", result);
